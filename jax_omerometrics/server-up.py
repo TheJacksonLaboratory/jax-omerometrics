a12affbd2b10be598dbfc21c961f4784ad620366
Erick Martins Ratamero <erickmartins@gmx.net>
2021-07-23 16:27:43 -0400

    added a few try-catches
    Just want to make sure `assert`s fail with `AssertionError` and not anything else

--- jax_omerometrics/server-up.py
+++ jax_omerometrics/server-up.py
@@ -1,20 +1,30 @@
 import ezomero
 import requests
 
-def check_web_response(address='htps://images.jax.org'):
+def check_web_response(address='https://images.jax.org'):
     r = requests.get(address)
     return r.status_code == requests.codes.ok
 
 
 def check_ldap_login(user, pwd, address='bhomero01lp.jax.org', port=4064, secure=True, group=''):
-    conn = ezomero.connect(user=user, password=pwd, host=address, port=port, secure=secure, group=group)
-    ret = (conn != None)
-    conn.close()
+    ret = False
+    try:
+        conn = ezomero.connect(user=user, password=pwd, host=address, port=port, secure=secure, group=group)
+        ret = (conn != None)
+        conn.close()
+    except AttributeError:
+        pass
     return ret
 
 
 def check_img_return(img_id, user, pwd, address='bhomero01lp.jax.org', port=4064, secure=True, group=''):
-    conn = ezomero.connect(user=user, password=pwd, host=address, port=port, secure=secure, group=group)
-    img, arr = ezomero.get_image(conn, img_id)
-    conn.close()
-    return img != None
+    ret = False
+    try:
+        conn = ezomero.connect(user=user, password=pwd, host=address, port=port, secure=secure, group=group)
+        img, _ = ezomero.get_image(conn, img_id)
+        conn.close()
+        ret = (img != None)
+    except AttributeError:
+        pass
+    return ret
+    
