e081acdea4a5bfd5d8949a18232f9301c2cfc20d
Erick Martins Ratamero <erickmartins@gmx.net>
2025-03-26 10:40:09 -0400

    using ezomero's create_json_session

--- jax_omerometrics/server_up.py
+++ jax_omerometrics/server_up.py
@@ -8,64 +8,12 @@ an OMERO server is up and running properly.
 import ezomero
 import requests
 import signal
+from ezomero import json_api
 
 
 def handler(signum, frame):
     """Handle signal."""
     raise Exception("Blitz connection timeout")
-
-
-def create_json_session(web_host, username, password, verify=True):
-    """Create a session for checking JSON API status."""
-    session = requests.Session()
-    # Start by getting supported versions from the base url...
-    api_url = '%s/api/' % web_host
-    r = session.get(api_url, verify=verify, timeout=5)
-    assert r.status_code == 200
-    print(api_url, r)
-    # we get a list of versions
-    versions = r.json()['data']
-    # use most recent version...
-    version = versions[-1]
-    # get the 'base' url
-    base_url = version['url:base']
-    r = session.get(base_url, timeout=5)
-    # which lists a bunch of urls as starting points
-    urls = r.json()
-    servers_url = urls['url:servers']
-    login_url = urls['url:login']
-
-    # To login we need to get CSRF token
-    token_url = urls['url:token']
-    token = session.get(token_url, timeout=5).json()['data']
-    print('CSRF token', token)
-    # We add this to our session header
-    # Needed for all POST, PUT, DELETE requests
-    session.headers.update({'X-CSRFToken': token,
-                            'Referer': login_url})
-
-    # List the servers available to connect to
-    servers = session.get(servers_url, timeout=5).json()['data']
-    SERVER_NAME = 'omero'
-    servers = [s for s in servers if s['server'] == SERVER_NAME]
-    if len(servers) < 1:
-        raise Exception("Found no server called '%s'" % SERVER_NAME)
-    server = servers[0]
-
-    # Login with username, password and token
-    payload = {'username': username,
-               'password': password,
-               # Using CSRFToken in header
-               'server': server['id']}
-
-    r = session.post(login_url, data=payload, timeout=5)
-    login_rsp = r.json()
-    assert r.status_code == 200
-    assert login_rsp['success']
-
-    # Can get our 'default' group
-
-    return login_rsp, session, base_url
 
 
 def check_web_response(address):
@@ -82,7 +30,7 @@ def check_web_api(user, pwd, img_id, address):
     print("checking json api...")
     print(address, user)
     try:
-        _, session, base_url = create_json_session(address, user, pwd)
+        _, session, base_url = json_api.create_json_session(web_host=address, user=user, password=pwd)
         print("created session successfully")
     except Exception as e:
         print(f"{type(e).__name__} at line {e.__traceback__.tb_lineno} of {__file__}: {e}")
