a11a0520145ba41a2e8801c04db3c99219b0d8db
Kiya Govek <govekk@ctomerod01lp.jax.org>
2025-08-26 09:03:12 -0400

    upped repeats

--- collect_hourly.py
+++ collect_hourly.py
@@ -134,7 +134,7 @@ if __name__ == "__main__":
                         default="ctomero01lp.jax.org",
                         help='Address for OMERO server instance')
     args = parser.parse_args()
-    repeats = 1
+    repeats = 5
     print(args.img_id)
     status, timing = collect_data(repeats, OMERO_USER, OMERO_PASS, args.img_id,
                                   args.web_addr, args.addr)
